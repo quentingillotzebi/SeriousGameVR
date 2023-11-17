63d23b701f49b9105ede0bbf076f4f658b881e88
Antoine DELFOSSE <ant.delfosse@live.fr>
2023-11-17 10:28:31 +0100

    Antoine D new conveyor

--- Scripts/ConveyorBelt.cs
+++ Scripts/ConveyorBelt.cs
@@ -2,53 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConveyorBelt : MonoBehaviour
+public class ConveyorNew : MonoBehaviour
 {
-    [SerializeField]
-    private float speed, conveyorSpeed;
-    [SerializeField]
-    private Vector3 direction;
-    [SerializeField]
-    private List<GameObject> onBelt;
-
-    private Material material;
-
+    public float speed, materialSpeed;
+    Rigidbody rBody;
+    Material material;
     // Start is called before the first frame update
     void Start()
     {
-        /* Create an instance of this texture
-         * This should only be necessary if the belts are using the same material and are moving different speeds
-         */
+        rBody = GetComponent<Rigidbody>();
         material = GetComponent<MeshRenderer>().material;
     }
 
     // Update is called once per frame
-    private void Update()
-    {
-        // Move the conveyor belt texture to make it look like it's moving
-        material.mainTextureOffset += new Vector2(0, 1) * conveyorSpeed * Time.deltaTime;
-    }
-
-    // Fixed update for physics
     void FixedUpdate()
     {
-        // For every item on the belt, add force to it in the direction given
-        for (int i = 0; i <= onBelt.Count - 1; i++)
-        {
-            onBelt[i].GetComponent<Rigidbody>().AddForce(speed * direction);
-            //onBelt[i].GetComponent<Rigidbody>()
-        }
-    }
+        Vector3 pos = rBody.position;
+        rBody.position += Vector3.right * speed * Time.fixedDeltaTime;
+        rBody.MovePosition(pos);
 
-    // When something collides with the belt
-    private void OnCollisionEnter(Collision collision)
-    {
-        onBelt.Add(collision.gameObject);
-    }
-
-    // When something leaves the belt
-    private void OnCollisionExit(Collision collision)
-    {
-        onBelt.Remove(collision.gameObject);
+        material.mainTextureOffset += new Vector2(0,-1) * materialSpeed * Time.deltaTime;
     }
 }
